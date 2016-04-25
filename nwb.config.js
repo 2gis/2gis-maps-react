f36c4541eeccc584764865713ba30fe565fecde6
vincent <cherebedov.sergey@gmail.com>
2016-04-25 14:38:42 +0600

    Enable babel stage0 (ES7 support)

--- nwb.config.js
+++ nwb.config.js
@@ -1,11 +1,15 @@
 module.exports = {
-  type: 'react-component',
-  build: {
-    externals: {
-      'react': 'React'
+    type: 'react-component',
+    build: {
+        externals: {
+            'react': 'React'
+        },
+        global: 'y',
+        jsNext: true,
+        umd: true
     },
-    global: 'y',
-    jsNext: true,
-    umd: true
-  }
-}
+    babel: {
+        stage: 0,
+        optional: ['runtime']
+    }
+};
